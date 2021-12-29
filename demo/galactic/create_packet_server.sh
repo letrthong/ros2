75d854ae0a53ace19b1076d5f19d989d28d35c29
Thong LT <letrthong@gmail.com>
2021-12-29 17:14:30 +0700

    Update create_packet_server.sh

--- demo/galactic/create_packet_server.sh
+++ demo/galactic/create_packet_server.sh
@@ -1,2 +1,3 @@
 source /opt/ros/galactic/setup.bash
+#ros2 pkg create --build-type ament_cmake  cpp_srvcli  --dependencies rclcpp  example_interfaces
 ros2 pkg create --build-type ament_cmake  cpp_srvcli  --dependencies rclcpp  example_interfaces
