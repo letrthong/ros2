50b288da066c3f552d314c08c8ea4ecf0607c44b
Thong LT <letrthong@gmail.com>
2021-12-31 14:40:45 +0700

    Client

--- demo/galactic/01.client/src/add_two_ints_client.cpp
+++ demo/galactic/01.client/src/add_two_ints_client.cpp
@@ -4,6 +4,45 @@
 // https://docs.ros.org/en/foxy/Tutorials/Writing-A-Simple-Cpp-Publisher-And-Subscriber.html#write-the-subscriber-node
 #include "std_msgs/msg/string.hpp"
 using std::placeholders::_1;
+#include "example_interfaces/srv/add_two_ints.hpp"
+
+#include <thread>
+
+
+void thread_function()
+{ 
+ 
+    std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("add_two_ints_client");
+    rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedPtr client =
+        node->create_client<example_interfaces::srv::AddTwoInts>("add_two_ints");
+
+    auto request = std::make_shared<example_interfaces::srv::AddTwoInts::Request>();
+    request->a =3;
+    request->b = 5;
+
+    while (!client->wait_for_service(1s)) {
+        if (!rclcpp::ok()) {
+            RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
+            return 0;
+        }
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
+    }
+
+    auto result = client->async_send_request(request);
+    // Wait for the result.
+    if (rclcpp::spin_until_future_complete(node, result) ==
+        rclcpp::FutureReturnCode::SUCCESS)
+    {
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Sum: %ld", result.get()->sum);
+    }
+    else {
+        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service add_two_ints");
+    }
+
+    rclcpp::shutdown();
+}
+
+
 
 class MinimalSubscriber : public rclcpp::Node
 {
@@ -24,7 +63,9 @@ private:
 };
 
 int main(int argc, char* argv[])
-{
+{  
+
+    std::thread threadObj(thread_function);
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<MinimalSubscriber>());
     rclcpp::shutdown();
