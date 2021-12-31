08396b48031a0547e88742220a3c9db736621840
Thong LT <letrthong@gmail.com>
2021-12-31 13:37:48 +0700

    Update publisher.cpp

--- demo/galactic/02.publisher/src/publisher.cpp
+++ demo/galactic/02.publisher/src/publisher.cpp
@@ -31,6 +31,8 @@ private:
         RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
         publisher_->publish(message);
     }
+
+
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
     size_t count_;
