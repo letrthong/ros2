9cb2c270cee5f2d5d0f7819ebd48c9ccb1516215
Thong LT <letrthong@gmail.com>
2022-01-04 11:44:02 +0700

    struct

--- demo/galactic/02.publisher/src/publisher.cpp
+++ demo/galactic/02.publisher/src/publisher.cpp
@@ -6,11 +6,21 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
+#include ""
+
 using namespace std::chrono_literals;
 //https://docs.ros.org/en/foxy/Tutorials/Writing-A-Simple-Cpp-Publisher-And-Subscriber.html#write-the-subscriber-node
 
 /* This example creates a subclass of Node and uses std::bind() to register a
 * member function as a callback from the timer. */
+
+
+
+typedef struct {
+    int type,
+    int data
+} Foo;
+
 
 class MinimalPublisher : public rclcpp::Node
 {
@@ -21,6 +31,11 @@ public:
         publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
         timer_ = this->create_wall_timer(
             500ms, std::bind(&MinimalPublisher::timer_callback, this));
+
+
+        publisher1_= this->create_publisher< Foo>("status", 10);
+        timer1_ = this->create_wall_timer(
+            5000ms, std::bind(&MinimalPublisher::timer_callback1, this));
     }
 
 private:
@@ -33,9 +48,24 @@ private:
     }
 
 
+    void timer_callback1()
+    {  
+        Foo foo;
+        foo.type = count_;
+        foo.data = count_;
+        publisher1_->publish(foo);
+    }
+
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
 
+    
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+
+
+    rclcpp::TimerBase::SharedPtr timer1_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher1_;
 
     size_t count_;
 };
