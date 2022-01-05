ecc449d4ad552fb07c41883e0ece1c7230bba9a2
Thong LT <letrthong@gmail.com>
2022-01-05 13:40:24 +0700

    log

--- demo/galactic/05.app/src/NodeClientServices.cpp
+++ demo/galactic/05.app/src/NodeClientServices.cpp
@@ -1,5 +1,6 @@
 #include "NodeClientServices.h"
-
+#include <iostream>
+using namespace std;
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 using std::placeholders::_1;
@@ -8,6 +9,7 @@ using std::placeholders::_1;
 using namespace std::chrono_literals;
 
 int NodeClientServices::getNavInfo() {
+    cout << "NodeClientServices::getNavInfo()" << endl;
     std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("add_two_ints_client");
     rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedPtr client =
         node->create_client<example_interfaces::srv::AddTwoInts>("add_two_ints");
