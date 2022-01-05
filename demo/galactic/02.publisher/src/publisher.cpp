a56a65e7bf87dcd9b64becea056002666c086b6b
Thong LT <letrthong@gmail.com>
2022-01-05 12:19:50 +0700

    Signal

--- demo/galactic/02.publisher/src/publisher.cpp
+++ demo/galactic/02.publisher/src/publisher.cpp
@@ -21,7 +21,7 @@ public:
     {
         publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
         timer_ = this->create_wall_timer(
-            500ms, std::bind(&MinimalPublisher::timer_callback, this));
+            5000ms, std::bind(&MinimalPublisher::timer_callback, this));
 
  
     }
